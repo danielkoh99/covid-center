6d6c8b3a1db4ce1d1752db38be58a35a53e2e4ac
Andrei Cosmin Stan <andreicosmin.stan@gmail.com>
2021-05-13 13:03:06 +0200

    Created project

---
+++ src/main/java/com/covidcenter/covidcenter/CovidcenterApplication.java
@@ -0,0 +1,13 @@
+package com.covidcenter.covidcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/covidcenter/covidcenter/CovidcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidcenter.covidcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
