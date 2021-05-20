135451d287da0fd119fdbda89d8be92edaabf332
SimonBucko <bucino.36@gmail.com>
2021-05-20 23:28:51 +0200

    hi

--- src/main/java/com/covidcenter/covidcenter/controller/GeneralController.java
+++ src/main/java/com/covidcenter/covidcenter/controller/GeneralController.java
@@ -3,6 +3,7 @@ package com.covidcenter.covidcenter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
  //this controller will be used for all shared functionalities of our system like login,logout
 @RestController
 public class GeneralController {
